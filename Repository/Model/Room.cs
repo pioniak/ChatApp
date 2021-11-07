ab22777e1c98b76ffa19cfd2cee33188e8d3a44d
pioniak <piotrek.golebniak@gmail.com>
2021-11-07 12:27:41 +0100

    Dodaj pliki projektów.

---
+++ Repository/Model/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class Room
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public User Creator { get; set; }
+        public ICollection<RoomParticipant> Participants { get; set; }
+    }
+}
